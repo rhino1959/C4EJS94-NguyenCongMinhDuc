4b4ec611835169199651a97872e2b91875ea2c84
ducnguyen1959 <ducncm99@gmail.com>
2020-08-20 08:35:57 +0700

    up

---
+++ Session3/HomeWork/HomeWork1/index2.js
@@ -0,0 +1,8 @@
+let a = 5;
+let b = 6;
+
+let c 
+a=c;
+c=b;
+a=b;
+ console.log (a,b)    
--- Session3/Hvideo/Read All Excercise/3/index.js
+++ Session3/Hvideo/Read All Excercise/3/index.js
@@ -1,4 +1,4 @@
 let film = ['Spider Man', 'Iron Man', 'Captain America', 'Thor'];
-for (let i = 0; i < film.length; i++){
-    console.log (film[i])
+for (let i = 0; i < film.length; i++) {
+    console.log(i + 1 + ". " + film[i])
 }
