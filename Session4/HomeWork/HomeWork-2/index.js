ca971225d694decd7bca9782e7e380103a325111
ducnguyen1959 <ducncm99@gmail.com>
2020-08-24 19:57:12 +0700

    up

--- Session4/HomeWork/HomeWork-2/index.js
+++ Session4/HomeWork/HomeWork-2/index.js
@@ -5,7 +5,5 @@ const task = {
     dueDate: '2019-10-08T18:00:24+0000',
     expectedHours: 0.5,
     };
-    console.log("use one line of code to destructure to obtain subject, dueDate and assignTo\
-    from this object:")
     let {subject, dueDate, assignTo} = task;
     console.log(subject,dueDate,assignTo);
--- Session4/Video/index.js
+++ Session4/Video/index.js
@@ -1,5 +1,7 @@
-let person = {
-    name: 'Duc',
-    age: 20, 
-    favs: ['gym', 'ball', 'candy']
-}
+const task = {
+    subject: 'Implement login feature',
+    createdBy: 'Hoang Ngoc Duc',
+    assignTo: 'Nguyen Phuong Nam',
+    dueDate: '2019-10-08T18:00:24+0000',
+    expectedHours: 0.5,
+    };
